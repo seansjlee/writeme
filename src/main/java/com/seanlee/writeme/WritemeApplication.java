6d46a75d3b3b0ada0e2b7d61adf11e8592280405
Sean Lee <90493212+seansjlee@users.noreply.github.com>
2025-07-29 17:17:06 +0900

    chore: initial commit

---
+++ src/main/java/com/seanlee/writeme/WritemeApplication.java
@@ -0,0 +1,13 @@
+package com.seanlee.writeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WritemeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WritemeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seanlee/writeme/WritemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seanlee.writeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WritemeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
